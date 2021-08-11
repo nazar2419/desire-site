1d05dac496a638a93029b0c60d06ebb4d6cc0499
nazar2419 <nazarx095@gmail.com>
2021-08-11 13:11:35 +0300

    Responsive website

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,15 @@ $(function () {
   $('.rightside-menu__close').on('click', function() {
     $('.rightside-menu').addClass('rightside-menu--close');
   });
+
+  if($(window).width() < 651) {
+    $('.works-path__item--measuring').appendTo($('.works-path__items-box'));
+  }
+
+  $('.header__btn-menu').on('click', function() {
+    $('.menu').toggleClass('menu--open');
+  });
+ 
   $('.top__slider').slick({
     dots: true,
     arrows: false,
@@ -17,6 +26,52 @@ $(function () {
     slidesToScroll: 10,
     dots: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings:{
+          slidesToShow: 8,
+          slidesToScroll: 8,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1511,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        }
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 840,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 651,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 376,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 
   $('.article-slider__box').slick({
